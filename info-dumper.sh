73b415d729849981c996d12093050189ad94ec50
K9T33NwthTookThis <127902233+K9T33NwthTookThis@users.noreply.github.com>
2025-07-15 21:45:32 +0100

    Update info-dumper.sh
    Add option to print output

--- info-dumper.sh
+++ info-dumper.sh
@@ -40,8 +40,10 @@ getent passwd | while IFS=: read -r name _ uid _ _ _ _; do
 
   awk -v user="$name" '{gsub(user, "[REDACTED]"); print}' /tmp/rpc-dump.txt | tee /tmp/rpc-dump-stripped.txt &>/dev/null
 done
-
-cat /tmp/rpc-dump-stripped.txt
+read -p "Print output? [y, N]: " -t 8 output
+if [$output == y]; then
+  cat /tmp/rpc-dump-stripped.txt
+fi
 rm /tmp/rpc-dump.txt
 
 printf "                                                          
