3a9d3e61a3d23cff0cd38f007030cfdd50ed6329
K9T33NwthTookThis <127902233+K9T33NwthTookThis@users.noreply.github.com>
2025-07-21 12:26:14 +0100

    Update main.sh
    comment out "print output" option until we figure out a fix

--- main.sh
+++ main.sh
@@ -41,10 +41,11 @@ getent passwd | while IFS=: read -r name _ uid _ _ _ _; do
   awk -v user="$name" '{gsub(user, "[REDACTED]"); print}' /tmp/rpc-dump.txt | tee /tmp/rpc-dump-stripped.txt &>/dev/null
 done
 
-read -rp "Print output? [y, N]: " -t 8 output
-if [[ $output == y ]]; then
-  cat /tmp/rpc-dump-stripped.txt
-fi
+# TODO: make print output option work
+#read -rp "Print output? [y, N]: " -t 8 output
+#if [[ $output == y ]]; then
+#  cat /tmp/rpc-dump-stripped.txt
+#fi
 
 rm /tmp/rpc-dump.txt
 
